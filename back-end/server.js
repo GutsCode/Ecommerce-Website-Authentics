ded3adc48d006fdfa1055d5fd03bbeeddd7ac086
unknown <marioruiramos99@gmail.com>
2022-03-29 15:48:19 +0100

    Add & Setup nodeJS(back-end) and React(Front-end)

---
+++ back-end/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+const port = 3001
+
+app.get("/", (req,res) => {
+    res.json({"data": ["User_1","User_2","User_3"] })
+})
+
+app.listen(port, () =>{ console.log("Server started on port 5000") })
