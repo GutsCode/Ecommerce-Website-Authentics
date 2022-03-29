f5e1a2edf4065bed9b2d8bf806e4aaba5bfb0511
unknown <marioruiramos99@gmail.com>
2022-03-29 16:10:14 +0100

    Connect Front-end to Back-end & Create a request of Data

--- back-end/server.js
+++ back-end/server.js
@@ -3,8 +3,8 @@ const app = express()
 
 const port = 3001
 
-app.get("/", (req,res) => {
+app.get("/api", (req,res) => {
     res.json({"data": ["User_1","User_2","User_3"] })
 })
 
-app.listen(port, () =>{ console.log("Server started on port 5000") })
+app.listen(port, () =>{ console.log("Server started on port",port) })
