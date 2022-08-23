eb8dfc911e5f48857bc3cd64f74e295cf1776556
Mario Ramos <marioruiramos99@gmail.com>
2022-08-23 15:47:48 +0100

    Authentics – Sneakers Shop

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 import '../styles/Header.css';
 
 const Header = () =>{
-    return <div className="wapper">
+    return (
+    <div className="wapper">
         <div className="logoContainer">
              <div className="logoText">Authentics</div>
         </div>
         <div className="searchBar">
-
+            <div className="searchBarIcon">
+                <AiOutlineSearch/ >
+            </div>
         </div>
     </div>
+    )
 }
 
 export default Header
